618370a32e6e2fe463e675a6b8f49749ad54c459
speedstarkiwi <121459772+speedstarkiwi@users.noreply.github.com>
2023-04-07 12:46:25 -0400

    Update support.cpp

--- support.cpp
+++ support.cpp
@@ -1,1 +1,15 @@
-int execute(int rL, std::string script){}
+int execute(int rL, std::string script){
+
+    std::string str = script;
+    std::string oldStr = "game:HttpGet";
+    std::string newStr = "HttpGet";
+    
+    size_t pos = str.find(oldStr);
+    if (pos != std::string::npos) { 
+        str.replace(pos, oldStr.length(), newStr); 
+    }
+   
+    str.clear();
+  
+    //execute new string called 'str'
+}
